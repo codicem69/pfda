91eb7db810f7653b22d67600e478164de1d549a1
tommaso <codicem69@gmail.com>
2022-03-16 16:49:13 +0100

    aggiunto nuovo menu in istanceconfig

--- packages/pfda/menu.py
+++ packages/pfda/menu.py
@@ -1,22 +1,21 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+# encoding: utf-8
+class Menu(object):
+    def config(self,root,**kwargs):
+        proforma_da = root.branch(u"proforma da", tags="")
+        proforma_da.thpage(u"clienti", table="pfda.cliente", tags="", formResource="FormCliente")
+        proforma_da.thpage(u"imbarcazione", table="pfda.imbarcazione", tags="")
+        proforma_da.thpage(u"proforma", table="pfda.proforma", tags="", viewResource="ViewProforma")
+        proforma_da.thpage(u"Tariffe servizi portuali", table="pfda.tariffeportuali", tags="")
+        proforma_da.thpage(u"tariffe", table="pfda.tariffe", tags="")
+        proforma_da.thpage(u"tariffe_tipo", table="pfda.tariffa_tipo", tags="")
+        proforma_da.thpage(u"ormeggiatori", table="pfda.ormeggiatori", tags="")
+        proforma_da.thpage(u"agency", table="pfda.agency", tags="")
+        proforma_da.thpage(u"servizi", table="pfda.servizi", tags="")
+        proforma_da.thpage(u"tug", table="pfda.tug", tags="")
+        proforma_da.thpage(u"pilota", table="pfda.pilota", tags="")
+        proforma_da.thpage(u"serviziextra", table="pfda.serviziextra", tags="")
+        proforma_da.thpage(u"customs", table="pfda.customs", tags="")
+        proforma_da.thpage(u"Adm Charge", table="pfda.admcharge", tags="")
+        proforma_da.thpage(u"Antifire", table="pfda.antifire", tags="")
+        proforma_da.lookups(u"Tabelle Ausiliarie", lookup_manager="pfda")
 
-def config(root,application=None):
-    pfda = root.branch('proforma da')
-    pfda.thpage('clienti',table='pfda.cliente', formResource='FormCliente')
-    pfda.thpage('imbarcazione',table='pfda.imbarcazione')
-    pfda.thpage('proforma',table='pfda.proforma', viewResource='ViewProforma')
-    pfda.thpage('Tariffe servizi portuali',table='pfda.tariffeportuali')
-    pfda.thpage('tariffe',table='pfda.tariffe')
-    pfda.thpage('tariffe_tipo',table='pfda.tariffa_tipo')
-    pfda.thpage('ormeggiatori',table='pfda.ormeggiatori')
-    pfda.thpage('agency',table='pfda.agency')
-    pfda.thpage('servizi',table='pfda.servizi')
-    pfda.thpage('tug',table='pfda.tug')
-    pfda.thpage('pilota',table='pfda.pilota')
-    pfda.thpage('serviziextra',table='pfda.serviziextra')
-    pfda.thpage('customs',table='pfda.customs')
-    pfda.thpage('Adm Charge',table='pfda.admcharge')
-    pfda.thpage('Antifire',table='pfda.antifire')
-    pfda.lookups(u"Tabelle Ausiliarie", lookup_manager="pfda")
-   
