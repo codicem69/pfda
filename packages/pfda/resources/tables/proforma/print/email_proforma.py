add40d2fd30ded4963ba63c3bbd4be4a78facbb4
tommaso <codicem69@gmail.com>
2023-08-02 18:32:10 +0200

    modificato le risorse print del proforma per stampare con le giuste carte intestate

--- packages/pfda/resources/tables/proforma/print/email_proforma.py
+++ packages/pfda/resources/tables/proforma/print/email_proforma.py
@@ -12,13 +12,30 @@ class Main(BaseResourcePrint):
     batch_immediate='download'
     #Con batch_immediate='print' viene immediatamente aperta la stampa alla conclusione
     html_res = 'html_res/stampaprof'
-    templates = 'Ranalli_st'
+    #templates = 'Ranalli_st'
     mail_address='@cliente_id.email'
     batch_print_modes = ['pdf','mail_deliver']
-      
     #Non utilizziamo il table_script_parameters_pane perché ci limiteremo a stampare la selezione corrente
     
-    
+   # def table_script_parameters_pane(self, pane,**kwargs):
+   #     ag_id = self.db.currentEnv.get('current_agency_id')
+   #     print(ag_id)
+     #   fb = pane.formbuilder(cols=1, width='220px')
+     #   fb.dbselect('^.letterhead_id',table='adm.htmltemplate',lbl='carta intestata',hasDownArrow=True)
+    def pre_process(self):
+        ag_id = self.db.currentEnv.get('current_agency_id')
+
+        tbl_agency =  self.db.table('agz.agency')
+        htmltemplate_id = tbl_agency.readColumns(columns='htmltemplate_id',
+                  where='$id=:ag_id',
+                    ag_id=ag_id)
+        tbl_htmltemplate = self.db.table('adm.htmltemplate')
+        carta_intestata = tbl_htmltemplate.readColumns(columns='name',
+                            where='$id=:htmltemplate_id', htmltemplate_id=htmltemplate_id)
+        #templates = 'Ranalli_st'
+        self.templates = carta_intestata
+        self.htmlMaker = self.page.site.loadTableScript(page=self.page, table='pfda.proforma',
+                                                        respath='html_res/stampaprof', class_name='Main')
     def onRecordPrinted(self, record, filepath=None):
         tbl_proforma = self.db.table('pfda.proforma')
         if not record: 
--- packages/pfda/resources/tables/proforma/print/stampa_proforma.py
+++ packages/pfda/resources/tables/proforma/print/stampa_proforma.py
@@ -8,6 +8,24 @@ class Main(BaseResourcePrint):
     batch_immediate='print'
     #Con batch_immediate='print' viene immediatamente aperta la stampa alla conclusione
     html_res = 'html_res/stampaprof'
-    templates = 'Ranalli_st'
+    #templates = 'Ranalli_st'
 
     #Non utilizziamo il table_script_parameters_pane perché ci limiteremo a stampare la selezione corrente
+    #def table_script_parameters_pane(self, pane,**kwargs):
+    #    fb = pane.formbuilder(cols=1, width='220px')
+    #    fb.dbselect('^.letterhead_id',table='adm.htmltemplate',lbl='carta intestata',hasDownArrow=True)
+
+    def pre_process(self):
+        ag_id = self.db.currentEnv.get('current_agency_id')
+
+        tbl_agency =  self.db.table('agz.agency')
+        htmltemplate_id = tbl_agency.readColumns(columns='htmltemplate_id',
+                  where='$id=:ag_id',
+                    ag_id=ag_id)
+        tbl_htmltemplate = self.db.table('adm.htmltemplate')
+        carta_intestata = tbl_htmltemplate.readColumns(columns='name',
+                            where='$id=:htmltemplate_id', htmltemplate_id=htmltemplate_id)
+        #templates = 'Ranalli_st'
+        self.templates = carta_intestata
+        self.htmlMaker = self.page.site.loadTableScript(page=self.page, table='pfda.proforma',
+                                                        respath='html_res/stampaprof', class_name='Main')
