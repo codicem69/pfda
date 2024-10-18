842105119c39684ae1757aae287c2160f490c44c
tommaso <codicem69@gmail.com>
2024-10-18 16:15:12 +0200

    aggiunto validate_case='u' al nome imbarcazione per avere il nome in maiuscolo

--- packages/pfda/model/imbarcazione.py
+++ packages/pfda/model/imbarcazione.py
@@ -8,7 +8,7 @@ class Table(object):
         #tbl.column('tipo',name_long='Tipo')
         tbl.column('tip_imbarcazione_code', group='_', name_long='!![en]Vessel type',batch_assign=dict(hasDownArrow=True)
                     ).relation('tip_imbarcazione.code', relation_name='', mode='foreignkey', onDelete='raise')
-        tbl.column('nome',name_long='Nome')
+        tbl.column('nome',name_long='Nome', validate_case='u')
         tbl.column('bandiera',name_long='Bandiera')
         tbl.column('flag',size='2',name_long='!![en]Flag').relation('unlocode.nazione.code',relation_name='flag_pfda', mode='foreignkey', onDelete='raise')
         tbl.column('loa',name_long='LOA')
