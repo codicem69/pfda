ee961ea8be0deba51708213416e7f5506987fae7
codicem69 <codicem69@gmail.com>
2025-07-15 11:25:31 +0200

    modificato sequenza record proforma e full_cliente e agency

--- packages/pfda/model/agency.py
+++ packages/pfda/model/agency.py
@@ -8,6 +8,6 @@ class Table(object):
         tbl.column('proforma_id',size='22',name_long='proforma_id').relation('proforma.id',relation_name='proforma_agency', mode='foreignkey', onDelete='cascade')
         tbl.column('tariffe_id',size='22',name_long='tariffa agency').relation('tariffe.id',relation_name='tariffe_agency', mode='foreignkey', onDelete='setnull')
         tbl.column('quantita',dtype='I',name_long='Quantità')
-        tbl.column('sconto',dtype='N',size='3',name_long='Sconto %')
+        tbl.column('sconto',dtype='N',size='10,2',name_long='Sconto %')
         tbl.column('pu', dtype='N', size='10,2', name_long='P.U.',format='#,###.00')
         tbl.column('totagency',dtype='N',size='10,2',name_long='Totale Agency',format='#,###.00')
